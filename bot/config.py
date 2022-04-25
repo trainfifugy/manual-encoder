a8d5c0056452422b3de8c02ed119897939c749ea
trainfifugy <96743071+trainfifugy@users.noreply.github.com>
2022-04-25 11:58:25 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,11 +4,11 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
+    APP_ID = config("5168062", cast=int)
+    API_HASH = config("04c049aa96d1cc87920b45b7fb43c0d0")
     BOT_TOKEN = "5058183358:AAEE65GnRhfFi-DLb_3BX7V0m-QB4FRtIr8"
     DEV = 1664850827
-    OWNER = config("OWNER")
+    OWNER = config("1956035465")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/f9e5d783542906418412d.jpg")
 except Exception as e:
